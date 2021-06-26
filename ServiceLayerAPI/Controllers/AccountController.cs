8b03fb08388635c60804d2bdad7798ebcb5e7e2e
rahul <rahul@DESKTOP-FBQIK6Q>
2021-06-26 16:52:49 +0530

    Identity. Authorization Added at controller level

--- ServiceLayerAPI/Controllers/AccountController.cs
+++ ServiceLayerAPI/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using CALforDataTransfer.Models;
 using CALforDataTransfer.ViewModels;
 using DAL.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -14,57 +15,28 @@ namespace ServiceLayerAPI.Controllers
 
     [Route("api/[controller]/[Action]")]
     [ApiController]
+    [Authorize]
     public class AccountController:Controller
     {
-        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly UserManager<AppUser> userManager;
 
         public BStudentRepository bsObj { get; }
         public BTeacherRepository btObj { get; }
         public BCommonRepository bcObj { get; }
-        public AccountController(IStudentRepository iStudent, ITeacherRepository iTeacher, ICommonRepository iCommon,SignInManager<IdentityUser> signInManager)
+        public AccountController(IStudentRepository iStudent, ITeacherRepository iTeacher, ICommonRepository iCommon,SignInManager<AppUser> signInManager,
+                                    UserManager<AppUser> userManager)
         {
             bsObj = new BStudentRepository(iStudent);
             btObj = new BTeacherRepository(iTeacher);
             bcObj = new BCommonRepository(iCommon);
             this.signInManager = signInManager;
+            this.userManager = userManager;
         }
-        #region [Methods without Using Identity]
+
+        #region[HttpPost Methods]
         [HttpPost]
-        public JsonResult RegisterStudent(CommonStudent student)
-        {
-            bool status = false;
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    status = bsObj.RegisterStudent(student);
-                }
-            }
-            catch
-            {
-                status = false;
-            }
-            return Json(status);
-        }
-        [HttpPost]
-        public JsonResult RegisterTeacher(CommonTeacher teacher)
-        {
-            bool status = false;
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    status = btObj.RegisterTeacher(teacher);
-                }
-            }
-            catch
-            {
-                status = false;
-            }
-            return Json(status);
-        }
-        #endregion
-        [HttpPost]
+        [AllowAnonymous]
         public async Task<JsonResult> Register(RegisterViewModel registerViewModel)
         {
             bool status = false;
@@ -72,17 +44,44 @@ namespace ServiceLayerAPI.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    status = await bcObj.Register(registerViewModel);   
+                    status = await bcObj.Register(registerViewModel);
+                    if (status == false)
+                    {
+                        ModelState.AddModelError("Error", "error while Registering and signing");
+                    }
                 }
             }
             catch(Exception ex)
             {
                 status = false;
-                ModelState.AddModelError("Error:",ex.ToString());
+                ModelState.AddModelError("Exception:",ex.ToString());
             }
             return Json(status);
         }
         [HttpPost]
+        public async Task<JsonResult> Update(UpdateViewModel updateViewModel)
+        {
+            bool status = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    status = await bcObj.Update(updateViewModel);
+                    if (status == false)
+                    {
+                        ModelState.AddModelError("Error","error while Updating");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                ModelState.AddModelError("Exception:", ex.ToString());
+            }
+            return Json(status);
+        }
+        [HttpPost]
+        [AllowAnonymous]
         public async Task<JsonResult> Login(LoginViewModel loginViewModel)
         {
             bool status = false;
@@ -96,13 +95,13 @@ namespace ServiceLayerAPI.Controllers
                         status = true;
                         return Json(status);
                     }
-                    ModelState.AddModelError("SignedIn Error:","SignedIn Failed");
+                    ModelState.AddModelError("SignedIn Error:","SignedIn Failed,check your password");
                 }
             }
             catch (Exception ex)
             {
                 status = false;
-                ModelState.AddModelError("Error:", ex.ToString());
+                ModelState.AddModelError("Exception:", ex.ToString());
             }
             return Json(status);
         }
@@ -122,9 +121,29 @@ namespace ServiceLayerAPI.Controllers
             }
             return Json(status);
         }
+        [HttpPost]
+        public async Task<JsonResult> Delete()
+        {
+            bool status = false;
+            try
+            {
+                var user = await userManager.GetUserAsync(User);
+                var result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    status = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+            }
+            return Json(status);
+        }
 
+        #endregion
         [HttpGet]
-        public  JsonResult IsSignedIn()
+        public  JsonResult IsSignedIn  ()
         {
             bool status = false;
             try
